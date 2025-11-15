f55a1b429fbcd8e493da75ad89bfd1751885e501
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2025-11-15 22:37:37 +0200

    Новий коментар до змін

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,8 @@
-function getElementWidth() {
-
+function getElementWidth(content, padding, border,) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
